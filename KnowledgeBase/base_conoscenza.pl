0bbb1c1ee24d1ab444109cb98d576124e9e96f84
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-10-20 22:59:42 +0200

    ProveDiDimostrazione

--- KnowledgeBase/base_conoscenza.pl
+++ KnowledgeBase/base_conoscenza.pl
@@ -4,6 +4,14 @@ ok_l1.
 ok_l2.
 ok_cb1.
 ok_cb2.
+dark_l1.
+dark_l2.
+up_s1.
+down_s1.
+up_s2.
+down_s2.
+up_s3.
+down_s3.
 live_outside.
 live_l1 :- live_w0.
 live_w0 :- live_w1 , up_s2.
@@ -17,6 +25,7 @@ live_w3 :- live_w5 , ok_cb1.
 live_p2 :- live_w6.
 live_w6 :- live_w5 , ok_cb2.
 live_w5 :- live_outside.
-live_w5(Reason) :- live_outside, Reason = 'Perche sta bene'.
 lit_l1 :- light_l1 , live_l1 , ok_l1.
 lit_l2 :- light_l2 , live_l2 , ok_l2.
+false :- dark_l1 , lit_l1
+false :- dark_l2 , lit_l2
--- KnowledgeBase/main.py
+++ KnowledgeBase/main.py
@@ -1,26 +1,25 @@
 from pyswip import Prolog
+from KB import *
 
-# Inizializza il motore Prolog
-prolog = Prolog()
+kb = KB()
+with open("base_conoscenza.pl", "r") as file:
+    for riga in file:
+        cl=riga.split(" :- ")
+        body=[]
+        if len(cl)>1:
+            for atomo in cl[1].split(" , ") : body.append(atomo)
+            body[-1]=body[-1].replace(".\n","")
+            body[-1]=body[-1].replace(".","")
+        else:
+            cl[0]=cl[0].replace(".\n","")
+        kb.addClausola(clausola(cl[0],body))
+kb.addAssumable("ok_l1")
+kb.addAssumable("ok_l2")
+kb.addAssumable("ok_s1")
+kb.addAssumable("ok_s2")
+kb.addAssumable("ok_s3")
+kb.addAssumable("ok_cb1")
+kb.addAssumable("ok_cb2")
+kb.addAssumable("live_outside")
+print(kb.conflicts())
 
-# Carica la base di conoscenza dal file .pl
-prolog.consult("base_conoscenza.pl")
-
-
-
-def verifica_conseguenza(proposizione):
-    query = f"not {proposizione}."
-    if not prolog.query(f"not {query}."):
-        return "false"
-    else:
-        return "true"
-
-# Esempi di verifiche
-proposizione1 = "ok_l1"
-proposizione2 = "down_s2"
-
-risultato1 = verifica_conseguenza(proposizione1)
-risultato2 = verifica_conseguenza(proposizione2)
-
-print(f"La proposizione '{proposizione1}' è una conseguenza logica: {risultato1}")
-print(f"La proposizione '{proposizione2}' è una conseguenza logica: {risultato2}")
